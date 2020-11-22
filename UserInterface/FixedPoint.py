6b8f8f07a041fa188f4775ae164177ba0fb28e7d
fsosap <fsosap@eafit.edu.co>
2020-11-22 00:32:56 -0500

    Added the images for the UI of Fixed point

---
+++ UserInterface/FixedPoint.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,300,300)
+    win.setWindowTitle("Suerte HPTAS")
+
+    label= QtWidgets.QLabel(win)
+    label.setText("Gonorrea")
+    label.move(120,180)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
+    
